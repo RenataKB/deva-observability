4160d93ae5a1b36e3c3572f811a871066fcc932f
Renata <renata.kb@gmail.com>
2023-02-13 22:01:01 -0300

    Inclusão rota renda-variavel

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
 @app.route('/renda-fixa')
+@metrics.counter('efetivacao_renda_fixa', 'Numero de papeis de renda fixa efetivados', labels={'tipo': 'ACOES'})
 def renda_fixa():
     return render_template('lista.html', titulo="Renda Fixa")
+
+@app.route('/renda-variavel')
+@metrics.counter('efetivacao_renda_variavel', 'Numero de papeis de renda variavel efetivados', labels={'tipo': 'ACOES'})
+def renda_variavel():
+    return render_template('lista.html', titulo="Renda Variável")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0") #, port=5001 #se quiser mudar a porta
