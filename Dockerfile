0e2e26c25037a52c8ba10f1d172ced1d67bfc4c5
Renata <renata.kb@gmail.com>
2023-03-06 15:22:30 -0300

    Adiciona rota no client e pasta static no Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.10.5-slim-buster
 COPY requirements.txt /requirements.txt
 COPY templates /templates
+COPY static /static
 COPY app.py /app.py
 RUN pip3 install -r requirements.txt
 CMD ["python", "app.py"]
--- client-app.py
+++ client-app.py
@@ -1,9 +1,8 @@
 import requests
 import random
 
-endpoints = ['renda-fixa', 'renda-variavel', 'fii', 'cripto']
+endpoints = ["renda-fixa", "renda-variavel", "fii", "cripto", ""]
 seguir_rodando = True
 
 while seguir_rodando:
-    requisicao = requests.get(f'http://app:5000/{random.choice(endpoints)}') #deu erro
-    # requisicao_daniel = requests.get('http://app:5000/'+endpoints[random.randint(0,3)])
+    requisicao = requests.get(f"http://app:5000/{random.choice(endpoints)}")
